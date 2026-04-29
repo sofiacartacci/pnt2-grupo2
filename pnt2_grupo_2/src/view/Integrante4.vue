91ae8690c187bb6ec523e89c373f7c7fb7a62fdf
scartacci <sofiabelencartacci@gmail.com>
2026-04-28 23:12:43 -0300

    integrante 4

---
+++ pnt2_grupo_2/src/view/Integrante4.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Integrante 1 - Eduardo Ramiro Calaza</h1>
+    <p>....</p>
+  </div>
+</template>
+
+<script setup>
+    //Agregar logica del componente 
+</script>
+
+<style scoped>
+</style>
