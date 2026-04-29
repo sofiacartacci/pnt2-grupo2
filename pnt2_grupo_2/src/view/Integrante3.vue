7e4dc4673f452e7ea24666d80eddd38d6206b619
Ramiro Calaza <ramiro.calaza@gmail.com>
2026-04-28 23:03:34 -0300

    wip

---
+++ pnt2_grupo_2/src/view/Integrante3.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Integrante 3 - Ferreyra Juan Gabriel</h1>
+    <p>....</p>
+  </div>
+</template>
+
+<script setup>
+    //Agregar logica del componente 
+</script>
+
+<style scoped>
+</style>
