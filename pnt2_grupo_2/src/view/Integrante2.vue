408956674f0cb8b4ea6cc81d238118de50d62cda
scartacci <sofiabelencartacci@gmail.com>
2026-04-28 22:48:06 -0300

    add integrante

---
+++ pnt2_grupo_2/src/view/Integrante2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Integrante 2 - Sofia Cartacci</h1>
+    <p>....</p>
+  </div>
+</template>
+
+<script setup>
+    //Agregar logica del componente 
+</script>
+
+<style scoped>
+</style>
