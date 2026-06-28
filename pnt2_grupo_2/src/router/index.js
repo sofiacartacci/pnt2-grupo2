19eb3e798ad0bd499bd9766c7ad31a5fbd35874a
scartacci <sofiabelencartacci@gmail.com>
2026-06-28 20:41:47 -0300

    delete integrante4

--- pnt2_grupo_2/src/router/index.js
+++ pnt2_grupo_2/src/router/index.js
@@ -5,7 +5,7 @@ import HomeView from "../view/HomeView.vue";
 import RegistroView from "../view/RegistroView.vue";
 import Peliculas from "../view/Peliculas.vue";
 import butacas from "../view/butacas.vue";
-import Integrante4 from "../view/cines.vue";
+import Cines from "../view/cines.vue";
 import AdminView from "../view/AdminView.vue";
 import SobreNosotros from "@/view/SobreNosotros.vue";
 import PoliticasPrivacidad from "@/view/PoliticasPrivacidad.vue";
@@ -30,7 +30,7 @@ const routes = [
   {
     path: "/cines",
     name: "Cines",
-    component: Integrante4,
+    component: Cines,
   },
   {
     path: "/butacas",
@@ -38,15 +38,6 @@ const routes = [
     component: butacas,
     meta: {
       requiereLogin: true,
-    },
-  },
-  {
-    path: "/integrante4",
-    name: "Integrante4",
-    component: Integrante4,
-    meta: {
-      requiereLogin: true,
-      requiereAdmin: true,
     },
   },
   {
